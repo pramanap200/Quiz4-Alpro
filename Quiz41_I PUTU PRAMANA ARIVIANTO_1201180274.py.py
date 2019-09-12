359a99c2f71673eeeaa937ff7f0d8585cc509e7d
pramanap200 <54703393+pramanap200@users.noreply.github.com>
2019-09-12 15:43:40 +0700

    Add files via upload

---
+++ Quiz41_I PUTU PRAMANA ARIVIANTO_1201180274.py.py
@@ -0,0 +1,6 @@
+ListMahasiswa = ["Anton", "Budi", "Doni", "Huda"]
+
+print(ListMahasiswa[0])
+print(ListMahasiswa[1])
+print(ListMahasiswa[2])
+print(ListMahasiswa[3])
---
+++ Quiz42_I PUTU PRAMANA ARIVIANTO_1201180274.py
@@ -0,0 +1,4 @@
+Fruits = ("nanas", "apel", "pepaya", "kentang")
+p = 0 
+for p in range (4) : 
+    print(Fruits[p])
